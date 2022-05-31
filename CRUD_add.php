421b9ecec6fe48deec9a7857fc73e0e84efe6a2c
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2022-05-31 17:49:09 -0300

    Add files via upload

--- CRUD_add.php
+++ CRUD_add.php
@@ -2,6 +2,33 @@
 	$NomeLista = $_GET["NomeLista"];
 	$Html = $_GET["Html"];
 	$Arvore = $_GET["Arvore"];
-	echo $Arvore;
-	echo $Html;
+	echo $Arvore . "<br>";
+	echo $Html . "<br>";
+	include "connection.php";
+	// Teste de existência
+	$sql = "SELECT NomeLista, Html FROM Task WHERE NomeLista = ? ";
+	$params = array(
+		$NomeLista
+		);
+	$sth = $conn->prepare($sql);
+	$sth->execute($params);
+	// Dados
+	$data = [
+			'NomeLista' => $NomeLista,
+			'Html' => $Html,
+			'Arvore' => $Arvore
+		];
+	
+	if( $row = $sth->fetch() ){
+		echo  "[{\"Id\":\"EXISTE\"}, {\"NomeLista\":\"" . $row['NomeLista'] . "\"}, {\"Html\":\"" . $row['Html'] . "\"}, {\"Arvore\":\"" . $row['Arvore'] . "\"}]";
+		$sql = "UPDATE Task SET Html = :Html, Arvore = :Arvore WHERE NomeLista = :NomeLista ";
+		$sth = $conn->prepare($sql);
+		$sth->execute($data);			
+		} else {
+		echo  "[{\"Id\":\"novo\"}, {\"NomeLista\":\"" . $NomeLista . "\"}, {\"Html\":\"" . $Html . "\"}]";
+		$sql = "INSERT INTO Task (NomeLista, Html, Arvore ) VALUES (:NomeLista, :Html, :Arvore)";
+		$sth = $conn->prepare($sql);
+		$sth->execute($data);
+		}
+		
 ?>
