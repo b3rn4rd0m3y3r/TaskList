aad0779e17c8659cc9ea6e870653574dd3ae5e0b
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2022-05-31 23:20:55 -0300

    Add files via upload

--- CRUD_add.php
+++ CRUD_add.php
@@ -3,8 +3,10 @@
 	$Html = $_GET["Html"];
 	$Arvore = $_GET["Arvore"];
 	echo $Arvore . "<br>";
-	echo $Html . "<br>";
+	echo $Html . "<br><br><br>";
+	
 	include "connection.php";
+	
 	// Teste de existência
 	$sql = "SELECT NomeLista, Html FROM Task WHERE NomeLista = ? ";
 	$params = array(
@@ -13,22 +15,26 @@
 	$sth = $conn->prepare($sql);
 	$sth->execute($params);
 	// Dados
+	
 	$data = [
 			'NomeLista' => $NomeLista,
 			'Html' => $Html,
 			'Arvore' => $Arvore
 		];
-	
+
 	if( $row = $sth->fetch() ){
 		echo  "[{\"Id\":\"EXISTE\"}, {\"NomeLista\":\"" . $row['NomeLista'] . "\"}, {\"Html\":\"" . $row['Html'] . "\"}, {\"Arvore\":\"" . $row['Arvore'] . "\"}]";
 		$sql = "UPDATE Task SET Html = :Html, Arvore = :Arvore WHERE NomeLista = :NomeLista ";
 		$sth = $conn->prepare($sql);
 		$sth->execute($data);			
 		} else {
-		echo  "[{\"Id\":\"novo\"}, {\"NomeLista\":\"" . $NomeLista . "\"}, {\"Html\":\"" . $Html . "\"}]";
+		echo  "[{\"Id\":\"novo\"}, {\"NomeLista\":\"" . $NomeLista . "\"}, {\"Html\":\"" . $Html . "\"}, {\"Arvore\":\"" . $Arvore . "\"}]";
 		$sql = "INSERT INTO Task (NomeLista, Html, Arvore ) VALUES (:NomeLista, :Html, :Arvore)";
-		$sth = $conn->prepare($sql);
-		$sth->execute($data);
+		try {
+			$sth = $conn->prepare($sql);
+			$sth->execute($data);
+		}catch(PDOException $e) {echo $e->getMessage();}
+
 		}
 		
 ?>
