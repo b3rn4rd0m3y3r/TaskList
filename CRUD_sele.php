2f359cefdd5e32253a3164fe79b1afee2d18e630
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2022-06-01 23:08:06 -0300

    Add files via upload

--- CRUD_sele.php
+++ CRUD_sele.php
@@ -1,20 +1,26 @@
 <?php
 	$NomeLista = $_GET["NomeLista"];
-	$Html = $_GET["Html"];
-	$Arvore = $_GET["Arvore"];
-	echo $Arvore . "<br>";
-	echo $Html . "<br>";
+	//$Html = $_GET["Html"];
+	//$Arvore = $_GET["Arvore"];
+	//echo $Arvore . "<br>";
+	//echo $Html . "<br>";
+	// CONEXÃO
 	include "connection.php";
-	// Teste de existência
-	$sql = "SELECT NomeLista, Html FROM Task WHERE NomeLista = ? ";
+	// Teste de existência e leitura dos campos
+	$sql = "SELECT NomeLista, Arvore, Html FROM Task WHERE NomeLista = ? ";
 	$params = array(
 		$NomeLista
 		);
 	$sth = $conn->prepare($sql);
 	$sth->execute($params);
 		if( $row = $sth->fetch() ){
+			$pattern = '/\"/';
+			$replacement = '\\"';
+			$row["Html"] = preg_replace($pattern, $replacement, $row["Html"]);
+			$row["Arvore"] = preg_replace($pattern, $replacement, $row["Arvore"]);
 			echo  "[{\"Id\":\"EXISTE\"}, {\"NomeLista\":\"" . $row['NomeLista'] . "\"}, {\"Html\":\"" . $row['Html'] . "\"}, {\"Arvore\":\"" . $row['Arvore'] . "\"}]";
 			} else {
 			echo  "[{\"Id\":\"NOVO\"}]";
 			}
-	close($conn);
+	$conn = null;
+?>
